558db590b863843d4cd61fb11dacb04a86f4b6bc
danCode-s <dancodes9@gmail.com>
2023-12-02 11:39:25 +0500

    worst function ever

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,16 @@
 print("Hello, World!")
+
+def enterName():
+    checker = 0
+    while (checker == 0):
+        name = input("What is your name: ")
+        if(type(name) != str):
+            checker = 0
+            return "Please Enter a String\n"
+        else:
+            checker = 1
+            return f"Welcome, {name}"
+        
+   
+print(enterName())
+    
